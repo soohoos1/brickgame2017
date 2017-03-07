b740b6259daa46b0e5d5efaa82b7bec4e4135fb1
Unknown <soohoos1@hawkmail.newpaltz.edu>
2017-03-07 13:58:37 -0500

    Added lives

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
-	public Vector2 startingVelocity = new Vector2 (5, -20);
+	public Vector2 startingVelocity = new Vector2 (15, -20);
+	private Vector3 startingPosition; 
+	public GameObject gameOver; 
+
+	int lives = 3; 
 
 	// Use this for initialization
 	void Start () {
+		startingPosition = transform.position; 
 		GetComponent<Rigidbody2D> ().velocity = startingVelocity; 
 	}
 
@@ -16,11 +21,28 @@ public class Ball : MonoBehaviour {
 		if (transform.position.y < -7.9F) {
 			GetOut ();
 		}
+		if (Input.GetButtonDown ("Jump"))
+			GetComponent<Rigidbody2D> ().velocity = startingVelocity;
 	}
 
 	void GetOut()
 	{
 		Debug.Log ("You are out");
 
+		lives = lives - 1; 
+
+		transform.position = startingPosition;
+		GetComponent<Rigidbody2D> ().velocity = new Vector2(); 
+
+
+		if (lives == 0) 
+		{
+			DoGameOver ();
+		}
+
+	}
+	void DoGameOver()
+	{
+		gameOver.SetActive(true); 
 	}
 }
