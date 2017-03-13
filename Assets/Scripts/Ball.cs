036b288a25973f212f18aa4588413c2048d7dad6
Unknown <soohoos1@hawkmail.newpaltz.edu>
2017-03-13 10:43:01 -0400

    Added Lives and Points Value Text

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class Ball : MonoBehaviour {
 
@@ -8,13 +9,17 @@ public class Ball : MonoBehaviour {
 	private Vector3 startingPosition; 
 	public GameObject gameOverSign; 
 	public GameObject youWinSign; 
+	public Text livesValue;
+	public Text pointsValue; 
 
 	int lives = 3; 
+	int points = 0; 
 
 	// Use this for initialization
 	void Start () {
 		startingPosition = transform.position; 
 		GetComponent<Rigidbody2D> ().velocity = startingVelocity; 
+		livesValue.text = lives.ToString (); 
 	}
 
 	// Update is called once per frame
@@ -32,6 +37,7 @@ public class Ball : MonoBehaviour {
 		Debug.Log ("You are out");
 
 		lives = lives - 1; 
+		livesValue.text = lives.ToString ();
 
 		transform.position = startingPosition;
 		GetComponent<Rigidbody2D> ().velocity = new Vector2 ();
@@ -49,6 +55,8 @@ public class Ball : MonoBehaviour {
 
 	public void YouBrokeABrick()
 	{
+		points += 1; 
+		pointsValue.text = points.ToString (); 
 		var bricksLeft = FindObjectsOfType<Brick>().Length; 
 		Debug.Log ("bricksleft: "+bricksLeft);
 		if (bricksLeft == 0) {
